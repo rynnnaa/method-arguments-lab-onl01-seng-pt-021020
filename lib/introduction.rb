26517c577fe126aeaea16724683cf1996f2aaacf
Ryna Truong <rynatruong@outlook.com>
2020-02-10 23:08:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,4 @@
-def introduction (name)
+def #introduction (name)
   puts "Hi, my name is #{name}."
 end
 
@@ -6,7 +6,7 @@ end
 
 
 
-def introduction_with_language (name, language)
+def #introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
